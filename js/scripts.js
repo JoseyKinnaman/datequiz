0b874712132f5a6b8c1385c21389b13741e2d7e7
dusty mccord and jozy kinnaman <dusty@>
2020-01-16 14:43:03 -0800

    Add heading and updated spacing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,9 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //buttons
     var humorBtn = $("#humor input");
     var messyBtn = $("#messy input");
     var drinkBtn = $("#drink input");
     var smartBtn = $("#smart input");
-    
 
     //answers
     var humorAnswer;
@@ -12,58 +11,49 @@ $(document).ready(function(){
     var drinkAnswer;
     var smartAnswer;
 
-    humorBtn.change(function(){
+    humorBtn.change(function () {
         humorAnswer = this.value;
         console.log(humorAnswer);
         $(".messy").show();
     });
 
-    messyBtn.change(function(){
+    messyBtn.change(function () {
         messyAnswer = this.value;
         console.log(messyAnswer);
 
         if (humorAnswer === "no" && messyAnswer === "yes") {
             hideAll();
             $("#cl").show();
-        } else if (humorAnswer=== "no" && messyAnswer === "no") {
+        } else if (humorAnswer === "no" && messyAnswer === "no") {
             hideAll();
             $("#flan").show();
-        }else if(humorAnswer === "yes" && messyAnswer === "no") {
+        } else if (humorAnswer === "yes" && messyAnswer === "no") {
             hideAll();
             $("#marge").show();
         } else {
             hideAll();
             $(".drink").show();
         }
-
     });
 
-    drinkBtn.change(function(){
+    drinkBtn.change(function () {
         drinkAnswer = this.value;
         console.log(drinkAnswer);
-            hideAll();
-            $(".smart").show();
-        
-
+        hideAll();
+        $(".smart").show();
     });
 
-    smartBtn.change(function(){
+    smartBtn.change(function () {
         smartAnswer = this.value;
         console.log(smartAnswer);
-        if (smartAnswer === "yes"){
+        if (smartAnswer === "yes") {
             hideAll();
             $("#kc").show();
         } else {
             hideAll();
             $("#homie").show();
         }
-        // if(smart ye) {
-        //     show krusty
-        // } else {
-        //     show homer
-        // }
     });
-
 
     function hideAll() {
         $('#images img').hide();
