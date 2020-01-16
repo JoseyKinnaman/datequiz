b461af9b0169aa87f9110f21025154bb68d6ccf9
dusty mccord and jozy kinnaman <dusty@>
2020-01-16 14:24:55 -0800

    add functionality to the quiz.

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
     var messyBtn = $("#messy input");
     var drinkBtn = $("#drink input");
     var smartBtn = $("#smart input");
+    
 
     //answers
     var humorAnswer;
@@ -14,9 +15,57 @@ $(document).ready(function(){
     humorBtn.change(function(){
         humorAnswer = this.value;
         console.log(humorAnswer);
+        $(".messy").show();
+    });
+
+    messyBtn.change(function(){
+        messyAnswer = this.value;
+        console.log(messyAnswer);
+
+        if (humorAnswer === "no" && messyAnswer === "yes") {
+            hideAll();
+            $("#cl").show();
+        } else if (humorAnswer=== "no" && messyAnswer === "no") {
+            hideAll();
+            $("#flan").show();
+        }else if(humorAnswer === "yes" && messyAnswer === "no") {
+            hideAll();
+            $("#marge").show();
+        } else {
+            hideAll();
+            $(".drink").show();
+        }
 
     });
 
+    drinkBtn.change(function(){
+        drinkAnswer = this.value;
+        console.log(drinkAnswer);
+            hideAll();
+            $(".smart").show();
+        
+
+    });
+
+    smartBtn.change(function(){
+        smartAnswer = this.value;
+        console.log(smartAnswer);
+        if (smartAnswer === "yes"){
+            hideAll();
+            $("#kc").show();
+        } else {
+            hideAll();
+            $("#homie").show();
+        }
+        // if(smart ye) {
+        //     show krusty
+        // } else {
+        //     show homer
+        // }
+    });
 
 
+    function hideAll() {
+        $('#images img').hide();
+    }
 });
